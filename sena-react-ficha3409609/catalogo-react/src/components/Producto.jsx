46ac6fca519038f6ef4743cc892fac883e7e015a
Sara Lucia Meneces Iregui <slvenegas@sagradoscorazonesmsq.edu.co>
2026-08-18 09:16:49 -0500

    Configuración inicial del proyecto React Vite

---
+++ sena-react-ficha3409609/catalogo-react/src/components/Producto.jsx
@@ -0,0 +1,10 @@
+export default function Producto() {
+  return (
+    <article className="producto">
+  <img src="/producto-demo.png" alt="Zapato deportivo" />
+  <h2>Nike Air Max</h2>
+  <p>Tenis deportivos para hombre.</p>
+  <strong>$450.000</strong>
+</article>
+  );
+}
